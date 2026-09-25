fcb9d20913d780a0d761d41faa5e570f57b4d764
Simon Fagerstedt <simon.fagerstedt@clxcommunications.com>
2026-09-25 18:44:26 +0200

    Update welcome message text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default async function Home() {
 
         {user ? (
           <p className="text-lg font-medium text-zinc-900 dark:text-zinc-50">
-            Välkommen {displayName}!
+            Välkommen till Vickleby bokcirkel {displayName}!
           </p>
         ) : (
           <p className="text-sm text-zinc-600 dark:text-zinc-400">
